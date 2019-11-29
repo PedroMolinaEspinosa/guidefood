31bb058cf16b8c1a23c58c8ee01f051fa63cab7c
pedromolina <pedro@aspgems.com>
2019-11-29 13:13:49 +0100

    Refactorizando con androidX

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:guidefood/src/controllers/controlador_pantalla..dart';
 import 'package:guidefood/src/routes/rutas.dart';
 import 'package:guidefood/src/styles/estilo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
